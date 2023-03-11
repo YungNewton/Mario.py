574bcadfefeb1d29484e511d5a143b4f5621305f
Isaac-Newton Ahanmisi <108007301+YungNewton@users.noreply.github.com>
2023-03-11 20:19:52 +0100

    Add files via upload

---
+++ mario2.py
@@ -0,0 +1,13 @@
+s = int(input("height: "))
+while s < 0 or s > 8:
+    s = int(input("height: "))
+g = 1
+d = s - 1
+for i in range(s):
+    while(g <= s):
+        print(" " * d , end = '')
+        print("#" * g , end = '')
+        print("  " , end = '')
+        print("#" * g)
+        g += 1
+        d -= 1
